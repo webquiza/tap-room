3495e497c7f0b68fe508085f83f1bc9450ede585
Carlos Urquiza <webquiza@gmail.com>
2022-01-16 12:02:49 -0800

    update BeerList.js to map to project objectives

---
+++ src/components/BeerList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Beer from "./Beer";
+import PropTypes from "prop-types";
+
+function BeerList(props){
+  return (
+    <React.Fragment>
+      {props.beerList.map((beer) => (
+        <Beer
+          whenBeerClicked = { props.onBeerSelection }
+          name={beer.name}
+          description={beer.description}
+          quantity={beer.quantity}
+          id={beer.id}
+          key={beer.id}  
+        />
+      ))}
+    </React.Fragment>
+  );
+}
+
+BeerList.propTypes = {
+  beerList: PropTypes.array,
+  onBeerSelection: PropTypes.func
+};
+
+export default BeerList;
